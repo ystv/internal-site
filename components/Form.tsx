7204153fdbac303af5935236caa5abb7f6813eff
Marks Polakovs <marks@markspolakovs.me>
2023-07-14 00:35:41 +0100

    Add TODO

--- components/Form.tsx
+++ components/Form.tsx
@@ -43,6 +43,10 @@ export default function Form<
       setIsSubmitting(true);
       let res;
       try {
+        // TODO: This submits the form values as JSON, effectively ignoring the FormData parameter. This works, but
+        //  may run into issues down the line, especially when (if?) we upload files directly from the form.
+        //  Instead, we should probably use the FormData object React gives us (though we'll have to figure out how
+        //  to make it play nice with hook-form).
         res = await action(form.getValues());
         if ("ok" in res && res.ok) {
           form.clearErrors();
