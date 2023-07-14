4bbee031ac1e5a6ae2091f0ad5edab25d45dd21e
Marks Polakovs <marks@markspolakovs.me>
2023-07-14 18:58:38 +0100

    Zod for validation

--- app/calendar/[eventID]/common.ts
+++ app/calendar/[eventID]/common.ts
@@ -1,4 +1,14 @@
-export const AttendStatusLabels = {
+export const AttendStatuses = [
+  "attending",
+  "not_attending",
+  "maybe_attending",
+  "invited",
+  "unknown",
+] as const;
+
+export const AttendStatusLabels: {
+  [K in (typeof AttendStatuses)[number]]: string;
+} = {
   attending: "Attending",
   not_attending: "Not Attending",
   maybe_attending: "Maybe Attending",
