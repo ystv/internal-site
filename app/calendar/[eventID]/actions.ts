6a5f6af2442f21b60e10db17723eccb085f7a9a3
Marks Polakovs <marks@markspolakovs.me>
2023-07-16 14:07:48 +0100

    Cleanup

--- app/calendar/[eventID]/actions.ts
+++ app/calendar/[eventID]/actions.ts
@@ -1,24 +1,24 @@
 "use server";
 import { getCurrentUser } from "@/lib/auth/legacy";
-import { prisma } from "@/lib/db";
 import { revalidatePath } from "next/cache";
-import { z } from "zod";
-import { zfd } from "zod-form-data";
-import { zodErrorResponse } from "@/components/FormServerHelpers";
 import { AttendStatus, AttendStatuses } from "@/features/calendar/statuses";
 import * as Calendar from "@/features/calendar";
 import { EventType, hasRSVP } from "@/features/calendar/types";
-
-const updateAttendeeStatusSchema = zfd.formData({
-  event_id: z.coerce.number(),
-  status: z.enum(AttendStatuses),
-});
 
 export async function updateAttendeeStatus(
   eventID: number,
   status: AttendStatus,
 ) {
   const me = await getCurrentUser();
+  // NB: this is an action, so we can't trust the status
+  if (!AttendStatuses.includes(status)) {
+    return {
+      ok: false,
+      errors: {
+        root: "Invalid status",
+      },
+    };
+  }
 
   const evt = await Calendar.getEvent(eventID);
   if (!evt) {
