79b90499c46731021d4f308186a8b62e96119183
Marks Polakovs <marks@markspolakovs.me>
2023-07-17 19:24:54 +0100

    Fix disabling debug mode

--- app/enableDebugMode/route.ts
+++ app/enableDebugMode/route.ts
@@ -5,7 +5,7 @@ export const DEBUG_MODE_COOKIE = "debugMode";
 
 export function GET(req: NextRequest): NextResponse {
   const val = req.nextUrl.searchParams.get("value")
-    ? Boolean(req.nextUrl.searchParams.get("value"))
+    ? req.nextUrl.searchParams.get("value") === "true"
     : true;
   cookies().set(DEBUG_MODE_COOKIE, String(val), {});
   return NextResponse.redirect(new URL("/", req.url));
