d3031f2acfa9eb39d7b6db37e072ee91dfa8e286
probablybenallen <26507833+probablybenallen@users.noreply.github.com>
2023-09-26 18:39:47 +0100

    fixed breadcrumbs

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -2,13 +2,13 @@
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import Link from "next/link";
-import { IoChevronForward } from "react-icons/io5";
+import { Anchor, Breadcrumbs } from "@mantine/core";
 
 const breadcrumbSegments: { [K: string]: string } = {
   calendar: "Calendar",
 };
 
-export default function Breadcrumbs() {
+export default function YSTVBreadcrumbs() {
   const path = usePathname();
   const segments = useMemo(() => {
     if (path === "/") {
@@ -22,40 +22,23 @@ export default function Breadcrumbs() {
         segments.push({ name: breadcrumbSegments[part], path: `/${part}` });
       }
     }
-    const result = [
-      <li key="/">
-        <Link href="/" className="text-gray-700 hover:text-blue-700">
-          Home
-        </Link>
-      </li>,
-    ];
+    const result = [];
     for (const segment of segments) {
       result.push(
-        <li key={"before-" + segment.path}>
-          <IoChevronForward />
-        </li>,
-      );
-      result.push(
-        <li key={segment.path}>
-          <Link
-            href={segment.path}
-            className="text-gray-700 hover:text-blue-700"
-          >
-            {segment.name}
-          </Link>
-        </li>,
+        <Anchor href={segment.path} component={Link} underline="hover">
+          {segment.name}
+        </Anchor>,
       );
     }
     return result;
   }, [path]);
 
-  if (segments.length === 0) {
-    return null;
-  }
-
   return (
-    <nav className="mx-2 my-2 flex items-center text-sm">
-      <ol className="flex items-center space-x-4">{segments}</ol>
-    </nav>
+    <Breadcrumbs>
+      <Anchor href="/" component={Link} underline="hover">
+        Home
+      </Anchor>
+      {segments}
+    </Breadcrumbs>
   );
 }
