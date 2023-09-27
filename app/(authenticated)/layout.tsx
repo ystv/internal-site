fb69b475b3c71ac1c825d6e56f80be6be12dab8b
probablybenallen <26507833+probablybenallen@users.noreply.github.com>
2023-09-27 16:04:19 +0100

    add constant little bit of space at the bottom of the page

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -40,6 +40,7 @@ export default async function AuthenticatedLayout({
       <main className="mx-2 max-w-[min(theme(maxWidth.6xl),theme(maxWidth.full))] md:mx-6 [@media(min-width:calc(theme(maxWidth.6xl)+theme(margin.6)*2))]:mx-auto">
         {children}
       </main>
+      <br />
     </UserProvider>
   );
 }
