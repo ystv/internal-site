afa97148b72e8e2da8e86bc04acfaf8b7abbcd35
probablybenallen <26507833+probablybenallen@users.noreply.github.com>
2023-10-08 19:34:47 +0100

    remove page break unless the add event button is being rendered

--- app/(authenticated)/calendar/page.tsx
+++ app/(authenticated)/calendar/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { PermissionGate } from "@/components/UserContext";
 import { listEventsForMonth } from "@/features/calendar/events";
 import { Button } from "@mantine/core";
+import { Permission } from "@/lib/auth/permissions";
 
 export default async function CalendarPage({
   searchParams,
@@ -19,29 +20,32 @@ export default async function CalendarPage({
   const day = searchParams.day ? parseInt(searchParams.day, 10) : now.getDate();
   const date = new Date(year, month, day);
   const events = await listEventsForMonth(year, month);
+
+  const calendarEditPermissions: Permission[] = [
+    "Calendar.Admin",
+    "Calendar.Show.Admin",
+    "Calendar.Show.Creator",
+    "Calendar.Meeting.Admin",
+    "Calendar.Meeting.Creator",
+    "Calendar.Social.Admin",
+    "Calendar.Social.Creator",
+    "Calendar.Show.Admin",
+    "Calendar.Social.Creator",
+  ];
+
   return (
     <>
       <div className={"flex items-end justify-between"}>
         <h1 className={"text-4xl font-bold"}>YSTV Calendar</h1>
-        <PermissionGate
-          required={[
-            "Calendar.Admin",
-            "Calendar.Show.Admin",
-            "Calendar.Show.Creator",
-            "Calendar.Meeting.Admin",
-            "Calendar.Meeting.Creator",
-            "Calendar.Social.Admin",
-            "Calendar.Social.Creator",
-            "Calendar.Show.Admin",
-            "Calendar.Social.Creator",
-          ]}
-        >
+        <PermissionGate required={calendarEditPermissions}>
           <Button component={Link} href="/calendar/new" fz="md">
             Add Event
           </Button>
         </PermissionGate>
       </div>
-      <br />
+      <PermissionGate required={calendarEditPermissions}>
+        <br />
+      </PermissionGate>
       <YSTVCalendar events={events} selectedDate={date} />
     </>
   );
