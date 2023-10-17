5dc5a754213c8cc2d4e386ce1782c9cd181c84d4
Marks Polakovs <marks.polakovs@ystv.co.uk>
2023-10-17 19:42:57 +0100

    [WEB-99] Tweak autocomplete state (#55)

--- app/(authenticated)/calendar/[eventID]/AddEditSignUpSheetForm.tsx
+++ app/(authenticated)/calendar/[eventID]/AddEditSignUpSheetForm.tsx
@@ -31,6 +31,7 @@ import {
   useCombobox,
 } from "@mantine/core";
 import { getUserName } from "@/components/UserHelpers";
+import invariant from "@/lib/invariant";
 
 /**
  * React component for a select input with custom options.
@@ -40,11 +41,12 @@ import { getUserName } from "@/components/UserHelpers";
  * * If true, `props.value` is assumed to be a custom value, and is displayed as-is. It is assumed that the parent component will handle storing it on the server.
  *
  * @component
- * @param {Object} props - Component props.
  * @param {Array} props.data - Array of objects with `label` and `value` properties for selectable options.
  * @param {string} props.value - Currently selected value (custom or from options).
  * @param {boolean} props.isCustomValue - Flag indicating if the selected value is custom.
- * @param {(value: string, isCustom: boolean) => unknown} props.onChange - Callback on value change.
+ * @param {(value: string, isCustom: boolean) => unknown} props.onChange - Callback on value change. If allowNone is true, the value will be "" if the user selects "None".
+ * @param {string} [props.placeholder] - Placeholder text for the input.
+ * @param {boolean} [props.allowNone] - If true, a "None" option will be displayed when the input is empty.
  */
 function SelectWithCustomOption(props: {
   data: { label: string; value: string }[];
@@ -54,20 +56,32 @@ function SelectWithCustomOption(props: {
   placeholder?: string;
   allowNone?: boolean;
 }) {
+  if (props.isCustomValue) {
+    invariant(props.value !== null, "value is null but isCustomValue is true");
+  }
+
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
   });
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const [search, setSearch] = useState("");
+  // This is the value typed by the user.
+  // null means that they haven't typed anything, they've just selected a value,
+  // or they've just closed the dropdown - either way, they're not searching.
+  // empty-string means they've cleared the input (a valid state).
+  const [search, setSearch] = useState<string | null>(null);
 
   const filtered = useMemo(
     () =>
-      props.data.filter((x) =>
-        x.label.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
-      ),
+      // if search is empty, this will implicitly return all items
+      search
+        ? props.data.filter((x) =>
+            x.label.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+          )
+        : props.data,
     [props.data, search],
   );
+
   const selected = useMemo(
     () =>
       props.isCustomValue
@@ -88,12 +102,20 @@ function SelectWithCustomOption(props: {
       withinPortal={false}
       onOptionSubmit={(val) => {
         if (val === "$create") {
+          // we want to create a new field with the given input
+          // though note that we don't actually create it until the form as a whole
+          // is submitted, instead we hang on to it (or rather, we have the parent
+          // component hang on to it and pass it back to us along with isCustomValue=true)
+          invariant(search !== null, "selected $create but search is null");
           props.onChange(search, true);
         } else if (val === "$null") {
+          // the user has selected nothing
           props.onChange("", false);
         } else {
+          // the user has selected an existing option
           props.onChange(val, false);
         }
+        setSearch(null);
         combobox.closeDropdown();
       }}
     >
@@ -101,7 +123,7 @@ function SelectWithCustomOption(props: {
         <InputBase
           ref={inputRef}
           rightSection={<ComboboxChevron />}
-          value={search || selected || ""}
+          value={search === null ? selected || "" : search}
           onChange={(e) => {
             combobox.openDropdown();
             combobox.updateSelectedOptionIndex();
@@ -114,12 +136,12 @@ function SelectWithCustomOption(props: {
           }}
           onBlur={() => {
             combobox.closeDropdown();
-            setSearch(selected ? selected : "");
+            setSearch(selected ? selected : null);
           }}
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               e.preventDefault();
-              combobox.selectActiveOption();
+              combobox.clickSelectedOption();
             }
           }}
           placeholder={props.placeholder}
@@ -128,15 +150,18 @@ function SelectWithCustomOption(props: {
       </ComboboxTarget>
       <ComboboxDropdown>
         <ComboboxOptions>
-          {props.allowNone && search.trim().length === 0 && (
+          {props.allowNone && (!search || search.trim().length === 0) && (
             <ComboboxOption value={"$null"}>None</ComboboxOption>
           )}
           {options}
-          {filtered.length === 0 && search.trim().length > 0 && (
-            <ComboboxOption value="$create">
-              &apos;{search}&apos;
-            </ComboboxOption>
-          )}
+          {/* show the "create custom" option if the user has typed something and it doesn't match an existing item *exactly* */}
+          {/* (they may want to create a new item that's a substring of a pre-existing one - see WEB-99) */}
+          {(search?.trim().length ?? 0) > 0 &&
+            !filtered.some((x) => x.label === search?.trim()) && (
+              <ComboboxOption value="$create">
+                &apos;{search}&apos;
+              </ComboboxOption>
+            )}
         </ComboboxOptions>
       </ComboboxDropdown>
     </Combobox>
