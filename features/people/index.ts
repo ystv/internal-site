6162eb396f3172075cf1c630292095b47798e3df
probablybenallen <26507833+probablybenallen@users.noreply.github.com>
2023-10-19 16:43:52 +0100

    little fixes for consistency

--- features/people/index.ts
+++ features/people/index.ts
@@ -1,2 +1,2 @@
 export * from "./users";
-export * from "../../lib/db/preferences";
+export * from "./preferences";
--- features/people/users.ts
+++ features/people/users.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/db";
 import { z } from "zod";
-import { UserPreferencesSchema } from "../../lib/db/preferences";
+import { UserPreferencesSchema } from "@/lib/db/preferences";
 
 /**
  * Fields of a user object that we (usually) want to expose to the world.
