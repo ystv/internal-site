eea93b12dda8499beaf22135988ac80abfa7f638
probablybenallen <26507833+probablybenallen@users.noreply.github.com>
2023-10-19 18:50:02 +0100

    discover - client side filtering, also fixes WEB-122

--- app/(authenticated)/calendar/discover/DiscoverView.tsx
+++ app/(authenticated)/calendar/discover/DiscoverView.tsx
@@ -10,16 +10,13 @@ import {
   Tooltip,
 } from "@mantine/core";
 import { DateTime } from "@/components/DateTimeHelpers";
-import { compareAsc, isSameDay } from "date-fns";
+import { isSameDay } from "date-fns";
 import { useRouter } from "next/navigation";
-import {
-  CrewPositionType,
-  CrewType,
-  EventObjectType,
-} from "@/features/calendar";
+import { CrewType, EventObjectType } from "@/features/calendar";
 import { useState } from "react";
 import { TbArticle, TbFilter, TbInfoCircle } from "react-icons/tb";
 import { MyRoleSignUpModal } from "@/app/(authenticated)/calendar/[eventID]/SignupSheet";
+import { crewPositionsTypeWithAvailability } from "@/app/(authenticated)/calendar/discover/page";
 
 export function DiscoverView({
   vacantRoles,
@@ -27,7 +24,7 @@ export function DiscoverView({
   position,
 }: {
   vacantRoles: EventObjectType[];
-  crewPositions: CrewPositionType[];
+  crewPositions: crewPositionsTypeWithAvailability[];
   position?: number;
 }) {
   const router = useRouter();
@@ -68,12 +65,11 @@ export function DiscoverView({
         placeholder={"Filter vacancies by type..."}
         defaultValue={position?.toString(10) ?? null}
         leftSection={<TbFilter />}
-        data={crewPositions
-          .map((val) => ({
-            label: val.name,
-            value: val.position_id.toString(10),
-          }))
-          .filter(Boolean)}
+        data={crewPositions.map((val) => ({
+          label: val.name,
+          value: val.position_id.toString(10),
+          disabled: !val.available,
+        }))}
         onChange={(val) => {
           router.push(`/calendar/discover${val ? `?position=${val}` : ""}`);
         }}
@@ -88,78 +84,103 @@ export function DiscoverView({
         </p>
       )}
       <div className={"flex flex-row flex-wrap gap-4"}>
-        {vacantRoles.map((event) => (
-          <Paper
-            key={event.event_id}
-            shadow="sm"
-            radius="md"
-            withBorder
-            className="flex-grow-1 !flex w-full flex-col p-[var(--mantine-spacing-md)] md:w-[calc(50%-theme(gap.4)/2)] lg:flex-grow-0 lg:p-[var(--mantine-spacing-xl)]"
-          >
-            <h2 className={"m-0"}>{event.name}</h2>
-            <p className={"m-0 mb-2 text-sm"}>
-              <strong>
-                <DateTime
-                  val={event.start_date.toISOString()}
-                  format="datetime"
-                />
-                {" - "}
-                {isSameDay(event.start_date, event.end_date) ? (
-                  <DateTime val={event.end_date.toISOString()} format="time" />
-                ) : (
+        {vacantRoles
+          .filter(
+            (event) =>
+              !position ||
+              event.signup_sheets.some((signupSheet) =>
+                signupSheet.crews.some(
+                  (crew) => crew.positions.position_id === position,
+                ),
+              ),
+          )
+          .map((event) => (
+            <Paper
+              key={event.event_id}
+              shadow="sm"
+              radius="md"
+              withBorder
+              className="flex-grow-1 !flex w-full flex-col p-[var(--mantine-spacing-md)] md:w-[calc(50%-theme(gap.4)/2)] lg:flex-grow-0 lg:p-[var(--mantine-spacing-xl)]"
+            >
+              <h2 className={"m-0"}>{event.name}</h2>
+              <p className={"m-0 mb-2 text-sm"}>
+                <strong>
                   <DateTime
-                    val={event.end_date.toISOString()}
+                    val={event.start_date.toISOString()}
                     format="datetime"
                   />
-                )}
-              </strong>
-            </p>
-            {event.description && (
-              <p className={"m-0 text-sm"}>{event.description}</p>
-            )}
-            <Divider my="xs" label="Available Roles" labelPosition="center" />
-            {event.signup_sheets
-              .filter((sheet) => sheet.crews.length > 0)
-              .map((sheet) => (
-                <div key={sheet.signup_id}>
-                  <h3 className={"m-0 text-lg"}>{sheet.title}</h3>
-                  <p className={"m-0 text-xs"}>
+                  {" - "}
+                  {isSameDay(event.start_date, event.end_date) ? (
                     <DateTime
-                      val={sheet.arrival_time.toISOString()}
+                      val={event.end_date.toISOString()}
+                      format="time"
+                    />
+                  ) : (
+                    <DateTime
+                      val={event.end_date.toISOString()}
                       format="datetime"
-                    />{" "}
-                    -{" "}
-                    {isSameDay(sheet.arrival_time, sheet.end_time) ? (
+                    />
+                  )}
+                </strong>
+              </p>
+              {event.description && (
+                <p className={"m-0 text-sm"}>{event.description}</p>
+              )}
+              <Divider my="xs" label="Available Roles" labelPosition="center" />
+              {event.signup_sheets
+                .filter((sheet) => sheet.crews.length > 0)
+                .filter(
+                  (sheet) =>
+                    !position ||
+                    sheet.crews.some(
+                      (crew) => crew.positions.position_id === position,
+                    ),
+                )
+                .map((sheet) => (
+                  <div key={sheet.signup_id}>
+                    <h3 className={"m-0 text-lg"}>{sheet.title}</h3>
+                    <p className={"m-0 text-xs"}>
                       <DateTime
-                        val={sheet.end_time.toISOString()}
-                        format="time"
-                      />
-                    ) : (
-                      <DateTime
-                        val={sheet.end_time.toISOString()}
+                        val={sheet.arrival_time.toISOString()}
                         format="datetime"
-                      />
-                    )}
-                  </p>
-                  {sheet.crews.map((crew) => (
-                    <div key={crew.crew_id}>
-                      <li className={"ml-6 text-base"}>
-                        {crew.positions.name}
-                      </li>
-                    </div>
-                  ))}
-                </div>
-              ))}
-            <div className={"flex grow items-end justify-end"}>
-              <Button
-                onClick={() => router.push(`/calendar/${event.event_id}`)}
-                leftSection={<TbArticle />}
-              >
-                Event Details
-              </Button>
-            </div>
-          </Paper>
-        ))}
+                      />{" "}
+                      -{" "}
+                      {isSameDay(sheet.arrival_time, sheet.end_time) ? (
+                        <DateTime
+                          val={sheet.end_time.toISOString()}
+                          format="time"
+                        />
+                      ) : (
+                        <DateTime
+                          val={sheet.end_time.toISOString()}
+                          format="datetime"
+                        />
+                      )}
+                    </p>
+                    {sheet.crews
+                      .filter(
+                        (crew) =>
+                          !position || crew.positions.position_id === position,
+                      )
+                      .map((crew) => (
+                        <div key={crew.crew_id}>
+                          <li className={"ml-6 text-base"}>
+                            {crew.positions.name}
+                          </li>
+                        </div>
+                      ))}
+                  </div>
+                ))}
+              <div className={"flex grow items-end justify-end"}>
+                <Button
+                  onClick={() => router.push(`/calendar/${event.event_id}`)}
+                  leftSection={<TbArticle />}
+                >
+                  Event Details
+                </Button>
+              </div>
+            </Paper>
+          ))}
       </div>
       <Modal opened={roleInfoModal} onClose={() => setRoleInfoModal(false)}>
         {crewPosition && (
