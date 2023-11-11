b3473e855dbc481df69351b8374c054a11dfb163
Max Moir <max.moir@icloud.com>
2023-11-11 20:28:41 +0000

    [WEB-126] Another silly mistake Slack now checks it's enabled before starting

--- lib/slack/slackConnect.ts
+++ lib/slack/slackConnect.ts
@@ -6,21 +6,6 @@ declare global {
 
 let app = global.slack;
 
-if (!app) {
-  app = global.slack = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true,
-    appToken: process.env.SLACK_APP_TOKEN,
-  });
-
-  (async () => await app.start())();
-}
-
-async function slackConnect() {
-  return app;
-}
-
 function checkEnabled() {
   if (
     process.env.SLACK_ENABLED?.toLowerCase() === "true" &&
@@ -35,6 +20,21 @@ function checkEnabled() {
   return false;
 }
 
-export default slackConnect;
-
 export const isSlackEnabled = checkEnabled();
+
+if (!app && isSlackEnabled) {
+  app = global.slack = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN,
+  });
+
+  (async () => await app.start())();
+}
+
+async function slackConnect() {
+  return app;
+}
+
+export default slackConnect;
