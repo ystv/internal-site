3b619a8fc0d97fc72b464944782bd23ea40061cf
Max Moir <max.moir@icloud.com>
2023-11-11 20:01:29 +0000

    [WEB-126] SLACK_ENABLED env variable

--- lib/slack/slackConnect.ts
+++ lib/slack/slackConnect.ts
@@ -23,6 +23,7 @@ async function slackConnect() {
 
 function checkEnabled() {
   if (
+    process.env.SLACK_ENABLED?.toLowerCase() === 'true' &&
     process.env.SLACK_BOT_TOKEN &&
     process.env.SLACK_APP_TOKEN &&
     process.env.SLACK_SIGNING_SECRET &&
