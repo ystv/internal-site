f6b138eb27bfe4945064b0d6a48ab127ebc8bb12
Max Moir <max.moir@icloud.com>
2023-11-15 16:47:07 +0000

    [WEB-130] Default host selection to current user

--- app/(authenticated)/calendar/new/form.tsx
+++ app/(authenticated)/calendar/new/form.tsx
@@ -14,18 +14,20 @@ import {
 import { useRouter } from "next/navigation";
 import { EventType } from "@/features/calendar/types";
 import { identity } from "lodash";
+import { useCurrentUser } from "@/components/UserContext";
 
 export function CreateEventForm(props: {
   action: FormAction<{ id: number }>;
   permittedEventTypes: EventType[];
 }) {
   const router = useRouter();
+  const user = useCurrentUser()
   return (
     <Form
       action={props.action}
       schema={schema}
       onSuccess={(res) => router.push(`/calendar/${res.id}`)}
-      initialValues={{ type: "show" }}
+      initialValues={{ type: "show", host: user.user_id }}
     >
       <TextField name="name" label="Name" required placeholder={"New Event"} />
       <TextAreaField name="description" label="Description" />
