8b80f3722528c436df24bbff7af9ca48a1f853f9
Max Moir <max.moir@icloud.com>
2023-11-27 16:08:52 +0000

    Implement User Profile UI changes from slack PR

--- app/(authenticated)/user/[id]/page.tsx
+++ app/(authenticated)/user/[id]/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
-import { getCurrentUser, requirePermission } from "@/lib/auth/server";
+import { getCurrentUser, logout, requirePermission } from "@/lib/auth/server";
 import * as People from "@/features/people";
 import * as Calendar from "@/features/calendar";
 import { notFound } from "next/navigation";
 import { getUserName } from "@/components/UserHelpers";
-import Form from "@/components/Form";
-import { InputWrapper, SegmentedControl, Stack } from "@mantine/core";
+import {
+  Avatar,
+  Button,
+  Card,
+  CopyButton,
+  Group,
+  Skeleton,
+  Space,
+  Stack,
+} from "@mantine/core";
 import { UserPreferences } from "./UserPreferences";
 
 export default async function UserPage({ params }: { params: { id: string } }) {
@@ -27,31 +34,67 @@ export default async function UserPage({ params }: { params: { id: string } }) {
   const prefs = People.preferenceDefaults(user.preferences);
   return (
     <div>
-      <h1>
-        {user.avatar && (
-          <Image
-            src={user.avatar}
-            alt=""
-            width={96}
-            height={96}
-            className="max-h-[4.5rem] w-auto rounded-full py-2"
-          />
-        )}
-        {getUserName(user)}
-      </h1>
-      <h2>Preferences</h2>
-      <UserPreferences value={prefs} userID={user.user_id} />
-      <h2>Add Calendar to Google Calendar</h2>
-      <p>
-        Add this URL as a new calendar in Google Calendar:
-        <input
-          disabled
-          className="w-96"
-          value={`${process.env.PUBLIC_URL}/iCal/${await Calendar.encodeUserID(
-            user.user_id,
-          )}`}
-        />
-      </p>
+      <Card withBorder>
+        <Group>
+          {user.avatar && (
+            <>
+              <Avatar src={user.avatar} radius={28} size={56} />
+            </>
+          )}
+          <Stack gap={3}>
+            <h2 className="my-0">{getUserName(user)}</h2>
+            <h4 className="my-0 text-[--mantine-color-placeholder]">
+              {user.email}
+            </h4>
+          </Stack>
+          <form
+            action={async () => {
+              "use server";
+              logout();
+            }}
+            className="ml-auto"
+          >
+            <Button
+              variant="filled"
+              color="red"
+              className="ml-auto"
+              type="submit"
+            >
+              Sign Out
+            </Button>
+          </form>
+        </Group>
+      </Card>
+      <Space h={"md"} />
+      <Card withBorder>
+        <Stack gap={0}>
+          <h2 className="mt-0">Preferences</h2>
+          <UserPreferences value={prefs} userID={user.user_id} />
+        </Stack>
+      </Card>
+      <Space h={"md"} />
+      <Card withBorder>
+        <Group>
+          <Stack gap={0} className="w-full">
+            <h2 className="mt-0">Add Calendar to Google Calendar</h2>
+            <Stack gap={0}>
+              <p>Add this URL as a new calendar in Google Calendar:</p>
+              {await (async () => {
+                const link = `${
+                  process.env.PUBLIC_URL
+                }/iCal/${await Calendar.encodeUserID(user.user_id)}`;
+
+                return (
+                  <Group>
+                    <input disabled className="sm:max-w-96" value={link} />
+                    <ICalCopyButton link={link} />
+                  </Group>
+                );
+              })()}
+            </Stack>
+          </Stack>
+        </Group>
+      </Card>
     </div>
   );
 }
