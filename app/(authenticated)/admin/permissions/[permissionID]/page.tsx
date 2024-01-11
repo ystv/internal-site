3d0a1516951c6cf16c039c3e5581d0076f9a5952
Liam Burnand <liam.burnand@ystv.co.uk>
2024-01-11 00:33:10 +0000

    Made the permission page look slightly nicer

--- app/(authenticated)/admin/permissions/[permissionID]/page.tsx
+++ app/(authenticated)/admin/permissions/[permissionID]/page.tsx
@@ -1,6 +1,11 @@
 import {notFound} from "next/navigation";
 import {twMerge} from "tailwind-merge";
 import {getPermission} from "@/features/permission";
+import {
+    Card,
+    Group,
+    Stack,
+} from "@mantine/core";
 
 export default async function PermissionPage({params}: { params: { permissionID: string } }) {
     const permission = await getPermission(parseInt(params.permissionID, 10));
@@ -8,21 +13,26 @@ export default async function PermissionPage({params}: { params: { permissionID:
         notFound();
     }
     return (
-        <>
-            <div className="w-fit grow font-bold">
-                <h1 className={twMerge("text-4xl font-bold")}>
-                    {permission.name}
-                </h1>
-                <h4 className={twMerge("text-4xl font-bold")}>
-                    {permission.permission_id}
-                </h4>
-                <h4 className={twMerge("text-4xl font-bold")}>
-                    {permission.name}
-                </h4>
-                <h4 className={twMerge("text-4xl font-bold")}>
-                    {permission.description}
-                </h4>
-            </div>
-        </>
+        <div>
+            <Card withBorder>
+                <Group>
+                    <Stack gap={3}>
+                        <h1 className={twMerge("text-4xl font-bold")}>
+                            Role ({permission.name})
+                        </h1>
+                        <h4 className={twMerge("text-4xl font-bold")}>
+                            {permission.permission_id}
+                        </h4>
+                        <h4 className={twMerge("text-4xl font-bold")}>
+                            {permission.name}
+                        </h4>
+                        <h4 className={twMerge("text-4xl font-bold")}>
+                            {permission.description}
+                        </h4>
+                        {JSON.stringify(permission)}
+                    </Stack>
+                </Group>
+            </Card>
+        </div>
     );
 }
