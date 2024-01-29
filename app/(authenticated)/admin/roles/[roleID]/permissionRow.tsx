d38edbac5ffa9b246f8cdd054569867ff8a62f80
Liam Burnand <liam.burnand@ystv.co.uk>
2024-01-29 21:57:17 +0000

    Adding permissionRow.tsx

---
+++ app/(authenticated)/admin/roles/[roleID]/permissionRow.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+// import {PermissionType} from "@/features/permission";
+
+export function PermissionRow(
+  {
+    permission,
+  }: {
+    permission: string;
+  },
+  { key }: { key: string },
+) {
+  const router = useRouter();
+
+  return (
+    <>
+      <tr
+        key={key}
+        className={`divide-x-2 divide-y-0 divide-dashed divide-gray-200 text-sm font-semibold dark:divide-[--mantine-color-placeholder]`}
+        style={{ height: "40px" }}
+        onClick={() => router.push(`/admin/permissions/${permission}`)}
+      >
+        <td style={{ textAlign: "center" }}>{permission}</td>
+      </tr>
+    </>
+  );
+}
