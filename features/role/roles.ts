7b0182ce1d78c560fa42ff67502a96ea9268fafa
Liam Burnand <liam.burnand@ystv.co.uk>
2024-01-29 21:59:07 +0000

    Adding and modifying functions

--- features/role/roles.ts
+++ features/role/roles.ts
@@ -18,11 +18,71 @@ export async function getRoles(): Promise<RoleType[]> {
   return prisma.role.findMany();
 }
 
-export async function getRole(
-  roleID: number,
-): Promise<RoleType | null> {
+export async function getRole(roleID: number): Promise<RoleType | null> {
   return prisma.role.findUnique({
     where: { role_id: roleID },
+  });
+}
+
+export async function getPermissionsForRole(
+  roleID: number,
+): Promise<{ permission: string }[] | null> {
+  return prisma.rolePermission.findMany({
+    where: {
+      roles: {
+        role_permissions: {
+          some: {
+            role_id: roleID,
+          },
+        },
+      },
+    },
+    select: {
+      permission: true,
+      // permissions: {
+      //   select: {
+      //     name: true,
+      //   },
+      // },
+    },
+  });
+}
+
+export async function getUsersForRole(
+  roleID: number,
+): Promise<
+  | {
+      users: {
+        user_id: number;
+        first_name: string;
+        last_name: string;
+        nickname: string;
+        avatar: string;
+      };
+    }[]
+  | null
+> {
+  return prisma.roleMember.findMany({
+    where: {
+      roles: {
+        role_members: {
+          some: {
+            role_id: roleID,
+          },
+        },
+      },
+    },
+    select: {
+      users: {
+        select: {
+          user_id: true,
+          first_name: true,
+          last_name: true,
+          nickname: true,
+          avatar: true,
+        },
+      },
+    },
   });
 }
 
@@ -36,10 +96,7 @@ export async function addRole(role: Prisma.RoleCreateInput) {
   return newRole.role_id;
 }
 
-export async function editRole(
-  roleID: number,
-  role: Prisma.RoleUpdateInput,
-) {
+export async function editRole(roleID: number, role: Prisma.RoleUpdateInput) {
   await prisma.$transaction([
     prisma.role.update({
       where: {
