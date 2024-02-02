2e60a5eec1f7944e5f17e5348904c3ccccf31420
Liam Burnand <liam.burnand@ystv.co.uk>
2024-02-02 13:21:37 +0000

    Forgot to add RolePermissionSchema

--- app/(authenticated)/admin/roles/schema.ts
+++ app/(authenticated)/admin/roles/schema.ts
@@ -4,3 +4,8 @@ export const RoleSchema = z.object({
   name: z.string(),
   description: z.string().optional(),
 });
+
+export const RolePermissionSchema = z.object({
+  permission: z.string(),
+  roleID: z.number(),
+});
