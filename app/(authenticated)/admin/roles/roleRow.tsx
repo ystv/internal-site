15f3100e67a0673fbbc321f3c544761caafcbe46
Liam Burnand <liam.burnand@ystv.co.uk>
2024-02-09 21:55:37 +0000

    Using the Link tag instead of onClick

--- app/(authenticated)/admin/roles/roleRow.tsx
+++ app/(authenticated)/admin/roles/roleRow.tsx
@@ -1,21 +1,20 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { RoleType } from "@/features/role/roles";
+import Link from "next/link";
 
 export function RoleRow({ role }: { role: RoleType }) {
-  const router = useRouter();
-
   return (
     <>
       <tr
         key={role.role_id}
         className={`divide-x-2 divide-y-0 divide-dashed divide-gray-200 text-sm font-semibold dark:divide-[--mantine-color-placeholder]`}
-        onClick={() => router.push(`/admin/roles/${role.role_id}`)}
       >
-        <td style={{ textAlign: "center" }}>{role.role_id}</td>
-        <td style={{ textAlign: "center" }}>{role.name}</td>
-        <td style={{ textAlign: "center" }}>{role.description}</td>
+        <Link href={`/admin/roles/${role.role_id}`}>
+          <td style={{ textAlign: "center" }}>{role.role_id}</td>
+          <td style={{ textAlign: "center" }}>{role.name}</td>
+          <td style={{ textAlign: "center" }}>{role.description}</td>
+        </Link>
       </tr>
     </>
   );
