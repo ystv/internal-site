4ec775aed4e1f08870ec8bda4fc03c31f6c28bcf
Max Moir <max.moir@icloud.com>
2024-05-09 19:15:14 +0100

    Oh fuck forgot to migrate properly.

--- lib/db/migrations/20240508203852_remove_old_slack_id/migration.sql
+++ lib/db/migrations/20240508203852_remove_old_slack_id/migration.sql
@@ -4,5 +4,9 @@
   - You are about to drop the column `slack_user_id` on the `users` table. All the data in the column will be lost.
 
 */
+
+INSERT INTO "identities" ("user_id", "provider", "provider_key")
+SELECT "user_id", 'slack', "slack_user_id" FROM "users" WHERE "slack_user_id" IS NOT NULL;
+
 -- AlterTable
 ALTER TABLE "users" DROP COLUMN "slack_user_id";
