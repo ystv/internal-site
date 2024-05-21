03c976db7d9e7a5d907177c918e4c427da659a18
Max Moir <63592337+archessmn@users.noreply.github.com>
2024-05-21 19:06:04 +0100

    [WEB-89] Management page for positions (#111)
    * [WEB-89] Crew position management screen
    
    * Tidy up stuff
    
    * Shift to pagination context
    
    * Fixed client error caused by query state being undefined
    
    * Separated out the position card into its own component and tidied other code

---
+++ components/SearchBar.tsx
@@ -0,0 +1,43 @@
+import { ActionIcon, TextInput, Tooltip } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { IoClose } from "react-icons/io5";
+
+export function SearchBar(props: {
+  default: string | undefined;
+  onChange: (query: string | undefined) => void;
+  delay?: number;
+  label?: string;
+  withClear?: boolean;
+}) {
+  const [searchQueryState, setSearchQueryState] = useState<string>(
+    props.default ?? "",
+  );
+
+  useEffect(() => {
+    const delayInputTimeoutId = setTimeout(() => {
+      props.onChange(searchQueryState);
+    }, props.delay ?? 500);
+    return () => clearTimeout(delayInputTimeoutId);
+  }, [searchQueryState, props.delay]);
+
+  const isSearchEmpty = searchQueryState == "";
+
+  return (
+    <TextInput
+      label={props.label ?? "Search"}
+      rightSection={
+        props.withClear && (
+          <Tooltip label={"Clear search"} disabled={isSearchEmpty}>
+            <ActionIcon variant="subtle" color="red" disabled={isSearchEmpty}>
+              <IoClose onClick={() => setSearchQueryState("")} />
+            </ActionIcon>
+          </Tooltip>
+        )
+      }
+      onChange={async (event) => {
+        setSearchQueryState(event.currentTarget.value);
+      }}
+      value={searchQueryState}
+    />
+  );
+}
