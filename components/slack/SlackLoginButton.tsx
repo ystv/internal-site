17badc829b0a0f29e7fdbcd7ea4ebbf5023549cc
Mia Moir <mia@archess.mn>
2024-07-24 00:04:02 +0100

    Update SlackLoginButton

--- components/slack/SlackLoginButton.tsx
+++ components/slack/SlackLoginButton.tsx
@@ -20,13 +20,11 @@ export default function SlackLoginButton(props: {
       }&redirect_uri=${encodeURIComponent(
         publicURL +
           "/login/slack/callback" +
-          `${
-            props.redirect
-              ? "?redirect=" + props.redirect
-              : loginRedirect
-              ? "?redirect=" + loginRedirect
-              : ""
-          }`,
+          (props.redirect
+            ? "?redirect=" + props.redirect
+            : loginRedirect
+            ? "?redirect=" + loginRedirect
+            : ""),
       )}&scope=openid profile email`}
       style={{
         alignItems: "center",
