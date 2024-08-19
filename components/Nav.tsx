a6f84f220f3d2f1947f46d1a11724ba9821b59b7
Kory Anderson <koryanderson1909@gmail.com>
2024-08-19 20:33:09 +0100

    Nav Tweaks

---
+++ components/Nav.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { AppShell, Group, rem } from "@mantine/core";
+import { useHeadroom } from "@mantine/hooks";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/app/_assets/logo.png";
+import { UserMenu } from "@/components/UserMenu";
+import styles from "@/styles/Nav.module.css";
+import YSTVBreadcrumbs from "@/components/Breadcrumbs";
+
+interface NavProps {
+  children: React.ReactNode;
+  user: any;
+}
+
+export default function Nav({ children, user }: NavProps) {
+  const pinned = useHeadroom({ fixedAt: 100 });
+  return (
+    <AppShell
+      header={{ height: 80, collapsed: !pinned, offset: false }}
+      padding="md"
+      classNames={{ header: styles.header }}
+    >
+      <AppShell.Header bg-dark>
+        <Group h="100%" px="md">
+          <Link href="/">
+            <Image
+              src={Logo}
+              alt=""
+              placeholder="blur"
+              height={96}
+              className="max-h-[4.5rem] w-auto py-2"
+            />
+          </Link>
+          <YSTVBreadcrumbs />
+          <div className="ml-auto h-14 w-14 space-x-1">
+            <UserMenu userAvatar={user.avatar} />
+          </div>
+        </Group>
+      </AppShell.Header>
+
+      <AppShell.Main pt={`calc(${rem(80)} + var(--mantine-spacing-md))`}>
+        {children}
+      </AppShell.Main>
+    </AppShell>
+  );
+}
