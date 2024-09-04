650fbe88f6c61dc42d9e4e27a0d734786fd0bb9c
Kory Anderson <koryanderson1909@gmail.com>
2024-09-04 16:20:10 +0100

    Moves footer into nav to fix main body size issues

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -38,11 +38,6 @@ export default async function AuthenticatedLayout({
           <main className="mx-2 max-w-[min(theme(maxWidth.6xl),theme(maxWidth.full))] overflow-x-hidden md:mx-6 [@media(min-width:calc(theme(maxWidth.6xl)+theme(margin.6)*2))]:mx-auto">
             {children}
           </main>
-          <br />
-          <footer className="mt-8 text-center text-sm text-gray-500">
-            Calendar version {process.env.NEXT_PUBLIC_RELEASE}. Built and
-            maintained by the YSTV Computing Team. <FeedbackPrompt />
-          </footer>
           <style
             dangerouslySetInnerHTML={{
               __html: `
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { AppShell, Group, rem } from "@mantine/core";
+import { Anchor, AppShell, Group, rem, Text } from "@mantine/core";
 import { useHeadroom } from "@mantine/hooks";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,10 +14,13 @@ interface NavProps {
 }
 
 export default function Nav({ children, user }: NavProps) {
-  const pinned = useHeadroom({ fixedAt: 100 });
+  const headerPinned = useHeadroom({ fixedAt: 100 });
+  const footerPinned = useHeadroom({ fixedAt: 100 });
+
   return (
     <AppShell
-      header={{ height: 80, collapsed: !pinned, offset: false }}
+      header={{ height: 80, collapsed: !headerPinned, offset: false }}
+      footer={{ height: 60, collapsed: !headerPinned, offset: false }}
       padding="md"
       classNames={{ header: styles.header }}
     >
@@ -39,9 +42,27 @@ export default function Nav({ children, user }: NavProps) {
         </Group>
       </AppShell.Header>
 
-      <AppShell.Main pt={`calc(${rem(80)} + var(--mantine-spacing-md))`}>
+      <AppShell.Main
+        pt={`calc(${rem(80)} + var(--mantine-spacing-md))`}
+        h={`10vh - ${rem(80)} - var(--mantine-spacing-md)`}
+      >
         {children}
       </AppShell.Main>
+      <AppShell.Footer bg-dark="true">
+        <div className="flex h-full items-center justify-center">
+          <Text ta="center">
+            Calendar version {process.env.NEXT_PUBLIC_RELEASE}. Built and
+            maintained by the{" "}
+            <Anchor
+              href="https://ystv.slack.com/archives/C05UATQKUMA"
+              className="underline"
+            >
+              YSTV Computing Team
+            </Anchor>
+            .
+          </Text>
+        </div>
+      </AppShell.Footer>
     </AppShell>
   );
 }
