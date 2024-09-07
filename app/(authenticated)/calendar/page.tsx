55e4eb386c7a5901b268c858a041ff794d7f8c7e
Kory Anderson <koryanderson1909@gmail.com>
2024-09-07 01:59:53 +0100

    I should go to bed maybe

--- app/(authenticated)/calendar/page.tsx
+++ app/(authenticated)/calendar/page.tsx
@@ -104,6 +104,7 @@ export default async function CalendarPage({
 
   return (
     <>
+      <h1 className={"text-4xl font-bold"}>Calendar</h1>
       {vacantEventsCount > 0 && (
         <Alert
           variant={"outline"}
@@ -125,14 +126,15 @@ export default async function CalendarPage({
           </Button>
         </Alert>
       )}
-      <div className={"flex items-end justify-between"}>
-        <h1 className={"text-4xl font-bold"}>YSTV Calendar</h1>
-        <PermissionGate required={calendarEditPermissions}>
-          <Button component={Link} href="/calendar/new" fz="md">
-            Add Event
-          </Button>
-        </PermissionGate>
-      </div>
+
+      <PermissionGate required={calendarEditPermissions}>
+        <br />
+      </PermissionGate>
+      <PermissionGate required={calendarEditPermissions}>
+        <Button component={Link} href="/calendar/new" fz="md" my="md">
+          Add Event
+        </Button>
+      </PermissionGate>
       <PermissionGate required={calendarEditPermissions}>
         <br />
       </PermissionGate>
