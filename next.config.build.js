ac7815cf7bf2af5d1219948ac45356f908c34867
Marks Polakovs <marks@markspolakovs.me>
2024-09-10 19:05:54 +0100

    Re-enable sourcemap uploading to Sentry in builds

--- next.config.build.js
+++ next.config.build.js
@@ -1,4 +1,6 @@
 // This config file contains only the necessary next config needed in production
+const { withSentryConfig } = require("@sentry/nextjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -24,4 +26,21 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const sentryWebpackPluginOptions = {
+  org: "ystv",
+  project: "calendar2023",
+};
+
+const sentryOptions = {
+  // Upload additional client files (increases upload size)
+  widenClientFileUpload: true,
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+};
+
+module.exports = withSentryConfig(
+  nextConfig,
+  sentryWebpackPluginOptions,
+  sentryOptions,
+);
