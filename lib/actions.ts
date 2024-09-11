22bd5f25bfa018d22c38eb518a8b6cf7db4ecc0e
Marks Polakovs <marks@markspolakovs.me>
2024-09-11 22:45:54 +0100

    Set Sentry user before running action

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,4 +1,5 @@
 import * as Sentry from "@sentry/nextjs";
+import { getCurrentUserOrNull } from "./auth/server";
 
 /**
  * Wraps a server action with Sentry instrumentation.
@@ -7,6 +8,18 @@ export function wrapServerAction<T extends (...args: any[]) => Promise<any>>(
   name: string,
   fn: T,
 ): (...args: Parameters<T>) => Promise<Awaited<ReturnType<T>>> {
-  return async (...args: Parameters<T>) =>
-    await Sentry.withServerActionInstrumentation(name, () => fn(...args));
+  return async (...args: Parameters<T>) => {
+    const user = await getCurrentUserOrNull();
+    if (user && typeof user === "object") {
+      Sentry.setUser({
+        id: user.user_id,
+        email: user.email,
+      });
+    } else {
+      Sentry.setUser(null);
+    }
+    return await Sentry.withServerActionInstrumentation(name, () =>
+      fn(...args),
+    );
+  };
 }
