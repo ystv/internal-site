221a08e25687a5195cce3a61675471ddfd397832
Mia Moir <me@archess.mn>
2024-09-12 23:12:28 +0100

    Use global cache for Slack app

--- lib/slack/slackApiConnection.ts
+++ lib/slack/slackApiConnection.ts
@@ -1,19 +1,27 @@
 import { App } from "@slack/bolt";
 import { env } from "../env";
 
+declare global {
+  var slack: App | undefined; // This must be a `var` and not a `let / const`
+}
+
 export const isSlackEnabled = env.SLACK_ENABLED === "true";
 
 async function slackApiConnection() {
-  return new App({
-    token: env.SLACK_BOT_TOKEN,
-    signingSecret: env.SLACK_SIGNING_SECRET,
-    socketMode: true,
-    appToken: env.SLACK_APP_TOKEN,
-    redirectUri: `${env.PUBLIC_URL}/login/slack/callback`,
-    installerOptions: {
-      redirectUriPath: "/login/slack/callback",
-    },
-  });
+  if (!global.slack) {
+    global.slack = new App({
+      token: env.SLACK_BOT_TOKEN,
+      signingSecret: env.SLACK_SIGNING_SECRET,
+      socketMode: true,
+      appToken: env.SLACK_APP_TOKEN,
+      redirectUri: `${env.PUBLIC_URL}/login/slack/callback`,
+      installerOptions: {
+        redirectUriPath: "/login/slack/callback",
+      },
+    });
+  }
+
+  return global.slack;
 }
 
 export default slackApiConnection;
