715bbb549ca4fb1b6a1732d41573ae7542a8f7f4
Mia Moir <me@archess.mn>
2024-10-24 15:10:42 +0100

    Update colours to improve contrast

--- features/calendar/types.ts
+++ features/calendar/types.ts
@@ -12,11 +12,19 @@ export function hasRSVP(type: EventType): boolean {
   return type !== "show";
 }
 
+enum Colours {
+  Blue = "#0074E0",
+  Green = "#218721",
+  Orange = "#DB6F0A",
+  Pink = "#D82C7F",
+  Purple = "#800080",
+}
+
 export const EventColours: Record<EventType, string> = {
-  show: "#1E90FF", // Blue
-  meeting: "#32CD32", // Green
-  workshop: "#FFA500", // Orange
-  social: "#800080", // Purple
-  public: "#1E90FF", // Blue
-  other: "#FFC0CB", // Pink
+  show: Colours.Blue, // Blue
+  meeting: Colours.Green,
+  workshop: Colours.Orange,
+  social: Colours.Purple,
+  public: Colours.Blue,
+  other: Colours.Pink,
 };
