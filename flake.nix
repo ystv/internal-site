06799ded44826871e9a8a0c4d1bcffe1e4f0e90b
Mia Moir <me@archess.mn>
2025-08-27 22:06:53 +0100

    Do not set prisma env vars on darwin

--- flake.nix
+++ flake.nix
@@ -4,12 +4,19 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
+        isDarwin = pkgs.stdenv.isDarwin;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -19,9 +26,11 @@
             mkcert
           ];
 
-          PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-          PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-          PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+          env = pkgs.lib.optionalAttrs (!isDarwin) {
+            PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+            PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+            PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+          };
         };
       }
     );
